75c36cd2e26a52bef9d662cfb7a656d38482d10b
niveknaheehs <kevinofwatford@gmail.com>
2017-04-18 20:10:11 +0100

    GeoFence Class

--- app/src/androidTest/java/com/bignerdranch/android/geomag3/FirebaseTest.java
+++ app/src/androidTest/java/com/bignerdranch/android/geomag3/FirebaseTest.java
@@ -5,8 +5,16 @@ import android.location.Location;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+
+import com.firebase.client.Firebase;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
@@ -18,46 +26,29 @@ import static org.junit.Assert.assertEquals;
 @RunWith(AndroidJUnit4.class)
 public class FirebaseTest {
 
-    private static final  double POILatitude = 51.5225261374409;
-    private static final double  POILongditude = -0.13083979995587666;
-
-    private static final  double TestLatitudeInside = 51.5225261374409;
-    private static final double  TestLongditudeInside = -0.13083979995587666;
-
-
-    private static final  double TestLatitudeOutside = 50.5225261374409;
-    private static final double  TestLongditudeOutside = -0.13083979995587666;
+    private Firebase mFirebaseRef;
 
     @Test
-    public void useAppContext() throws Exception {
+    public void TestFB() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.bignerdranch.android.geomag3", appContext.getPackageName());
-    }
+        //Firebase.setAndroidContext(appContext);
+        Firebase.setAndroidContext(appContext);
 
-    @Test
-    public void InsideGeoFence() throws Exception {
+        mFirebaseRef =  new Firebase("https://geomag-fa9c7.firebaseio.com/Test");
 
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        String timeStampStr ;
+        Map<String,Object> values = new HashMap<>();
 
+        timeStampStr = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+        values.put("timestamp", timeStampStr);
+        values.put("Test", 123456);
 
-        GeoFence geoFence = new GeoFence();
-
-        // Set Current Location
-        Location location =  new Location("myProvider");
-        location.setLatitude(TestLatitudeInside);
-        location.setLongitude(TestLongditudeInside);
-
-        // Set Point of interest
-        geoFence.setPOILocation(POILongditude,POILatitude);
-
-        // Test if we are inside the geofence
-        int res = geoFence.insideGeoFence(location);
+        mFirebaseRef.push().setValue(values);
 
          //The  test passes if we are in the geofence
-        assertEquals(res, 1);
+        assertEquals(1, 1);
     }
 }
 
